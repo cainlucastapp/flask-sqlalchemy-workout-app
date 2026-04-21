5d31b5da4d01c267a208faa7920109a56a23c720
cainlucastapp <cainlucastapp@agentformula.com>
2026-04-21 13:49:39 -0700

    Add nullable=False constraints to Workout.date and duration_minutes

--- server/models.py
+++ server/models.py
@@ -19,8 +19,8 @@ class Workout(db.Model):
     __tablename__ = 'workouts'
 
     id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.Date)
-    duration_minutes = db.Column(db.Integer)
+    date = db.Column(db.Date, nullable=False)
+    duration_minutes = db.Column(db.Integer, nullable=False)
     notes = db.Column(db.Text)
 
     workout_exercises = db.relationship('WorkoutExercise', back_populates='workout')
