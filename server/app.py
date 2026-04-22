cafbac31ad34d35eefb8e430de1745d1dd518468
cainlucastapp <cainlucastapp@agentformula.com>
2026-04-22 14:26:07 -0700

    Scaffold all API endpoints

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify, request
 from flask_migrate import Migrate
 
 from models import *
@@ -11,7 +11,50 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-# Define Routes here
+
+# Workout Routes
+
+@app.route('/workouts', methods=['GET'])
+def get_workouts():
+    return make_response(jsonify({'message': 'list all workouts'}), 200)
+
+@app.route('/workouts/<int:id>', methods=['GET'])
+def get_workout(id):
+    return make_response(jsonify({'message': f'get workout {id}'}), 200)
+
+@app.route('/workouts', methods=['POST'])
+def create_workout():
+    return make_response(jsonify({'message': 'create workout'}), 201)
+
+@app.route('/workouts/<int:id>', methods=['DELETE'])
+def delete_workout(id):
+    return make_response(jsonify({'message': f'delete workout {id}'}), 200)
+
+
+# Exercise Routes
+
+@app.route('/exercises', methods=['GET'])
+def get_exercises():
+    return make_response(jsonify({'message': 'list all exercises'}), 200)
+
+@app.route('/exercises/<int:id>', methods=['GET'])
+def get_exercise(id):
+    return make_response(jsonify({'message': f'get exercise {id}'}), 200)
+
+@app.route('/exercises', methods=['POST'])
+def create_exercise():
+    return make_response(jsonify({'message': 'create exercise'}), 201)
+
+@app.route('/exercises/<int:id>', methods=['DELETE'])
+def delete_exercise(id):
+    return make_response(jsonify({'message': f'delete exercise {id}'}), 200)
+
+
+# WorkoutExercise Routes
+
+@app.route('/workouts/<int:workout_id>/exercises/<int:exercise_id>/workout_exercises', methods=['POST'])
+def create_workout_exercise(workout_id, exercise_id):
+    return make_response(jsonify({'message': f'add exercise {exercise_id} to workout {workout_id}'}), 201)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
