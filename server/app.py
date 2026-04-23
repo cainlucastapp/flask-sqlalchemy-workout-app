72c46b7d022021d98171795b50d26fb6803da8dc
cainlucastapp <cainlucastapp@agentformula.com>
2026-04-23 14:28:52 -0700

    Separated schemas into its own schemas.py from models.py

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@ from flask import Flask, make_response, jsonify, request
 from flask_migrate import Migrate
 
 from models import *
+from schemas import WorkoutSchema, ExerciseSchema, WorkoutExerciseSchema
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
--- server/models.py
+++ server/models.py
@@ -2,7 +2,6 @@ from wsgiref import validate
 
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
-from marshmallow import Schema, fields, validate
 db = SQLAlchemy()
 
 
@@ -88,30 +87,3 @@ class WorkoutExercise(db.Model):
         if value is not None and value <= 0:
             raise ValueError("duration_seconds must be greater than 0")
         return value
-    
-
-# Marshmallow Schemas
-class WorkoutExerciseSchema(Schema):
-    id = fields.Int(dump_only=True)
-    workout_id = fields.Int()
-    exercise_id = fields.Int()
-    reps = fields.Int(validate=validate.Range(min=1, error="Reps must be greater than 0"))
-    sets = fields.Int(validate=validate.Range(min=1, error="Sets must be greater than 0"))
-    duration_seconds = fields.Int(validate=validate.Range(min=1, error="Duration must be greater than 0"))
-
-class ExerciseSchema(Schema):
-    id = fields.Int(dump_only=True)
-    name = fields.Str(required=True, validate=validate.Length(min=1, error="Name cannot be empty"))
-    category = fields.Str(required=True, validate=validate.OneOf(
-        ['weight training', 'cardio', 'yoga', 'spin', 'pilates', 'crossfit', 'other'],
-        error="Invalid category"
-    ))
-    equipment_needed = fields.Bool(required=True)
-
-class WorkoutSchema(Schema):
-    id = fields.Int(dump_only=True)
-    date = fields.Date(required=True)
-    duration_minutes = fields.Int(required=True, validate=validate.Range(min=1, error="Duration must be greater than 0"))
-    notes = fields.Str()
-    exercises = fields.List(fields.Nested(ExerciseSchema), dump_only=True)
-    workout_exercises = fields.List(fields.Nested(WorkoutExerciseSchema), dump_only=True)
